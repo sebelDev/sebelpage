cf47802118e350b35b493eb61cf315efdb6cc541
sebeldev <seblecraft@gmail.com>
2026-04-06 10:08:34 +1000

    This place is like a computer

--- js/index.js
+++ js/index.js
@@ -4,6 +4,14 @@ import { ComputerGame } from "./puter.js"
 // Await the dom content to load
 await new Promise((resolve, reject) => {document.addEventListener("DOMContentLoaded", resolve)})
 
+// button
+const button = document.getElementById("button")
+const audio = new Audio("/assets/audio/uuugh uugh.mp3")
+button.addEventListener("mousedown", () => {
+    audio.currentTime = 0
+    audio.play()
+})
+
 // Setup the computer game
 const canvas = document.getElementById("puter")
 const puter = new ComputerGame(canvas)
